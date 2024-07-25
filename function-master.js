7e965b0087fb838c5ea3c255cab636ff5f397ed3
khamal22 <yyeah3198@gmail.com>
2024-07-25 15:49:38 -0500

    finsihed up to step 10 function master js

--- function-master.js
+++ function-master.js
@@ -3,54 +3,117 @@
 //////////////////////////////////////////////////////////////////////
 
 function objectValues(object) {
-    // code
-} 
+  // code
+  var box = []
+  for (var key in object) {
+    box.push(object[key])
+
+  }
+  return box
+}
 
 //////////////////////////////////////////////////////////////////////
 // Function 2 - Keys to String ///////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
 function keysToString(object) {
+  var box = [];
+  for (var key in object) {
+    // push key into the box array  
+    box.push(key)
+      ;
+  }
+  //need to join and create whitespace 
+  return box.join(' ')
 
 }
-
 //////////////////////////////////////////////////////////////////////
 // Function 3 - Values to String /////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
 function valuesToString(object) {
-    
+  var box = []
+  // use typeOf to determine what datatype 
+  for (var key in object) {
+    if (typeof object[key] === 'string') {
+      box.push(object[key]);
+    }
+  }
+  //check if object is string and if it is push the value
+  return box.join(' ');
 }
 
 //////////////////////////////////////////////////////////////////////
 // Function 4 - Array or Object //////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+/*
+I: one argument
+O:return 'array' if its an array and 'object' if its an object"
+C:
+E:
+*/
 function arrayOrObject(collection) {
-    
+  //check if collection is array 
+  if (Array.isArray(collection)) {
+    return 'array';
+    //check if collection is object and if collection is not equla to null
+  } else if (typeof collection === 'object' && collection !== null) {
+    return 'object';
+  }
+  return 'neither';
 }
 
 //////////////////////////////////////////////////////////////////////
 // Function 5 - Capitalize Word //////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
+/*
+I:Should take a string of one word
+O:return the word with its first letter capitalized"
+C:
+E:
+*/
 function capitalizeWord(string) {
-    
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 //////////////////////////////////////////////////////////////////////
 // Function 6 - Capitalize All Words /////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+/**
+ *I: Should take a string of words
+ *O:return a string with all the words capitalized
+ *C:
+ *E:
+ */
+
 
 function capitalizeAllWords(string) {
-    
+  var words = string.split(' ')
+
+  for(let i = 0; i < words.length; i++){
+    words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+  }
+  return words.join(' ');
+
+
 }
 
 //////////////////////////////////////////////////////////////////////
 // Function 7 - Welcome Message //////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+/*
+I: Should take an object with a name property
+O:return 'Welcome <Name>!'"
+C:
+E:
+*/
 function welcomeMessage(object) {
+  var cap = object['name'].charAt(0).toUpperCase() + object["name"].slice(1);
+
+  return 'Welcome ' + cap + '!';
+
+
 
 }
 
@@ -58,15 +121,39 @@ function welcomeMessage(object) {
 // Function 8 - Profile Info /////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
+/**
+ *I:Should take an object with a name an a species
+ *O:return '<Name> is a <Species>'"
+ *C:
+ *E: 
+ */
 function profileInfo(object) {
+  var n = object['name'].charAt(0).toUpperCase() + object["name"].slice(1);
+  var s = object['species'].charAt(0).toUpperCase() + object["species"].slice(1);
 
+      return n + ' is a ' + s;
 }
 
 //////////////////////////////////////////////////////////////////////
 // Function 9 - Maybe Noises /////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+/**
+ * I:Should take an object
+ * O:if this object has a noises array return them as a string separated by a space, if there are no noises return 'there are no noises'"
+ * C:
+ * E:
+ */
 function maybeNoises(object) {
+
+   // Check if the object has a noises property and if it's an array
+  if(Array.isArray(object.noises) && object.noises.length > 0){
+    // Join the noises array into a string separated by spaces
+    return object.noises.join(' ');
+    // Return the message if there are no noises
+  }else{
+    return 'there are no noises';
+  }
+
 
 }
 
@@ -82,7 +169,7 @@ function hasWord(string, word) {
 // Function 11 - Add Friend //////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-function addFriend (name, object) {
+function addFriend(name, object) {
 
 }
 
@@ -130,23 +217,23 @@ function dedup(array) {
 // DON'T REMOVE THIS CODE ////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-if((typeof process !== 'undefined') &&
-   (typeof process.versions.node !== 'undefined')) {
-    // here, export any references you need for tests //
-    module.exports.objectValues = objectValues;
-    module.exports.keysToString = keysToString;
-    module.exports.valuesToString = valuesToString;
-    module.exports.arrayOrObject = arrayOrObject;
-    module.exports.capitalizeWord = capitalizeWord;
-    module.exports.capitalizeAllWords = capitalizeAllWords;
-    module.exports.welcomeMessage = welcomeMessage;
-    module.exports.profileInfo = profileInfo;
-    module.exports.maybeNoises = maybeNoises;
-    module.exports.hasWord = hasWord;
-    module.exports.addFriend = addFriend;
-    module.exports.isFriend = isFriend;
-    module.exports.nonFriends = nonFriends;
-    module.exports.updateObject = updateObject;
-    module.exports.removeProperties = removeProperties;
-    module.exports.dedup = dedup;
+if ((typeof process !== 'undefined') &&
+  (typeof process.versions.node !== 'undefined')) {
+  // here, export any references you need for tests //
+  module.exports.objectValues = objectValues;
+  module.exports.keysToString = keysToString;
+  module.exports.valuesToString = valuesToString;
+  module.exports.arrayOrObject = arrayOrObject;
+  module.exports.capitalizeWord = capitalizeWord;
+  module.exports.capitalizeAllWords = capitalizeAllWords;
+  module.exports.welcomeMessage = welcomeMessage;
+  module.exports.profileInfo = profileInfo;
+  module.exports.maybeNoises = maybeNoises;
+  module.exports.hasWord = hasWord;
+  module.exports.addFriend = addFriend;
+  module.exports.isFriend = isFriend;
+  module.exports.nonFriends = nonFriends;
+  module.exports.updateObject = updateObject;
+  module.exports.removeProperties = removeProperties;
+  module.exports.dedup = dedup;
 }
